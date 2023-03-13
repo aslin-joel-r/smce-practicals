315fd5559327c73ba867afcdd4659afd70f5c547
aslin-r <aslinjoelr123@gmail.com>
2023-03-13 21:26:49 +0530

    completed 2nd and 3rd

---
+++ s6_Internet Programming lab(CS8561)/exp-3/creditcard.js
@@ -0,0 +1,22 @@
+function cardnumber(inputtxt)
+{
+    
+var cardno = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+if(inputtxt.value.match(cardno))
+{
+return true;
+}
+else
+{
+alert("Not a valid Visa credit card number!");
+return false;
+}
+}
+
+let a=16;
+function()=>
+{
+    console.log(a);
+}
+var d=5;
+var d=6
